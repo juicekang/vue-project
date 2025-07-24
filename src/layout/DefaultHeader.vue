097d445e91783fee35d8fb45846bedb613152a0c
강민주 <juicekang@chunjae.co.kr>
2025-07-24 16:23:20 +0900

    api 테스트중..

---
+++ src/layout/DefaultHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1>헤더입니다</h1>
+  </header>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+import movieAPI from '@/lib/axios'
+const movies = ref([])
+const searchWord = ref(null)
+onMounted(() => {
+  movieAPI
+    .get('', {
+      params: {
+        apikey: 'ab23f732',
+        s: 'parasite', // 검색어
+      },
+    })
+    .then((res) => {
+      console.log(res.data)
+      movies.value = res.data.Search
+      console.log(movies)
+    })
+    .catch((err) => {
+      console.error('오류 발생!', err)
+    })
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    {{ movies }}
   </main>
 </template>
